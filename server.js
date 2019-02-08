75aa4a7e01ce4359286254a7e778af8b38dd600b
chamakura nikhil reddy <chamakuranikhilreddy@gmail.com>
2019-02-07 22:04:09 -0500

    simplified server.js

--- server.js
+++ server.js
@@ -1,17 +1,102 @@
-console.log('it is a twitter-node');
 
-var Twit = require('twit');
 
-var T = new Twit({
-  consumer_key:         'RKqy2gtvdDrQQoTGqYgro6z32',
-  consumer_secret:      'uR8umnGLhO7gKlJ4c861fQPX2HyFwDj8L6yqs7UE5ZAdWRfshy',
-  access_token:         '817250584726077441-qrEDkBX7paUtvP4GOMxnXHQ7HatPZli',
-  access_token_secret:  'Sn00Ut8ERrk9DXapKYibscANU8BdWVhWwGbTSZuRTxSfK',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+ // Module dependencies
+
+
+const express = require('express');
+const hbs = require('hbs');
+const path = require("path");
+const http = require("http");
+const socketIO = require('socket.io');
+var app = express();
+
+//all environments
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+var server = http.createServer(app);
+
+
+var {mongoos} = require("./db/attendanceModel.js");
+var {Attendance} = require("./db/attendanceModel.js");
+var {event} = require("./db/attendanceModel.js");
+
+
+
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+  Attendance.find().then((todos)=>{
+      socket.emit('allSub',todos);
+        },(e)=>{
+              console.log('not working')
+    });
+    
+  socket.on('presub',(obj)=>{
+       eventReg(obj,'Present');
+     Attendance.findOneAndUpdate({subjectName:obj},{$inc:{present: +1}},{new:true},(err,doc)=>{
+      socket.emit('updatedSubject',doc);
+        if (err) {
+        console.log("something went wrong");
+      }
+     });
+    });
+
+    socket.on('abssub',(obj)=>{
+        eventReg(obj,'Absence');
+       Attendance.findOneAndUpdate({subjectName:obj},{$inc:{absence: +1}},{new:true},(err,doc)=>{
+        socket.emit('updatedSubject',doc);
+        if (err) {
+            console.log("something went wrong");
+        }
+       });
+    });
+
+    socket.on('addsubject',(obj)=>{
+      Attendance.save().then((obj)=>{
+          console.log("saved")
+        },(e)=>{
+          console.log('Unable to save subject');
+        });
+    })
 });
 
-T.get('search/tweets', { q: 'inida', count: 5 }, function(err, data, response) {
-  console.log(data.statuses[1].text);
-})
+var d = new Date();
 
+function eventReg(obj,event1){
+    var event2 = new event({
+        subjectName: obj,
+        event: event1,
+        date: d.getDate(),
+        month: d.getMonth()
+    });
+    event2.save().then((doc)=>{
+         console.log('saved todo', doc);
+        },(e)=>{
+         console.log('Unable to save todo');
+        });
+};
+//developement only
+
+app.get('/',(req,res)=>{
+	res.render('attend');
+});
+
+app.get('/fallout',(req,res)=>{
+  res.render('fallout');
+});
+
+
+app.get('/addsubject',(req,res)=>{
+  res.render('subject');
+});
+
+
+
+
+server.listen(3000,()=>{
+	console.log("server is running on 3000 ")
+});
+
+module.exports = {server};
